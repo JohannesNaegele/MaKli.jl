704bf39db8ef82a0ef42bd1cc5f2f98e424f852b
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-08 15:19:37 +0200

    dataframe ready

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MaKli
 
 [![Build Status](https://github.com/JohannesNaegele/MaKli.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/JohannesNaegele/MaKli.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+Package that implements course material of the lecture Numerics and Stochastics of Climate Models (Mannheim Klima).
--- exercises/exercise_01.jl
+++ exercises/exercise_01.jl
@@ -1,30 +1,43 @@
 using Pkg; Pkg.activate("./MaKli")
 using DataFrames
 using Gadfly
+using Pipe
 
-DataFrame(
-    "Process" => [
-        "collision of droplets during cloud formation",
-        "formation of convection cells",
-        "development of large-scale weather systems",
-        "persistence of pressure distributions",
-        "Southern Oscillation",
-        "troposphere-stratosphere exchange"
-    ],
-    "Characteristic Time Scale" => [
-        [1e-6, 1e-3],
-        [1e4, 1e5],
-        [1e4, 1e5],
-        [1e6, 1e6],
-        [1e7, 1e7],
-        [1e7, 1e8],
-    ],
-    "Characteristic Spatial Scale" => [
-        [1e-6, 1e-6],
-        [1e2, 1e4],
-        [1e6, 1e7],
-        [1e6, 1e7],
-        [1e7, 1e7],
-        [0.0, Inf]
-    ]
+include("components.jl")
+
+dfs = [atmosphere, hydrosphere, cryosphere, land_surface, biosphere]
+ids = ["atmosphere", "hydrosphere", "cryosphere", "land_surface", "biosphere"]
+
+for i in eachindex(dfs)
+    transform!(dfs[i], :Process => (x -> ids[i]) => :component)
+end
+
+df = vcat(atmosphere, hydrosphere, cryosphere, land_surface, biosphere)
+
+# @pipe atmosphere |>
+#     transform!(
+#         _,
+#         ["Characteristic Time Scale", "Characteristic Spatial Scale"] => ByRow()
+#     )
+
+process_repeated = repeat(atmosphere[!, "Process"], inner=4)
+
+# Flatten vector columns
+time_scale_flattened = vcat(atmosphere[!, "Characteristic Time Scale"]...)
+
+plot(
+    atmosphere,
+    x="Characteristic Time Scale",
+    y="Characteristic Spatial Scale",
+    # color=:id, 
+    alpha=[0.3],
+    linestyle=[:dash],
+    Geom.polygon(fill=true),
+    Scale.color_discrete,
+   Theme(line_width=2pt,
+   lowlight_color=identity,
+   discrete_highlight_color=identity)
 )
+
+reduce(vcat, [DataFrame(x=[1, 5, 5, 1].+d, y=[14, 14, 10, 10].-d, id=d+1) for d in 0:9])
+[DataFrame(x=[1, 5, 5, 1].+d, y=[14, 14, 10, 10].-d, id=d+1) for d in 0:9]
