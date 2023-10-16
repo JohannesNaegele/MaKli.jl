9f2a21cf323148bb2c8c213acb53093cbef503b8
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-16 02:26:09 +0200

    Fixed equations macro

--- src/helpers/equations.jl
+++ src/helpers/equations.jl
@@ -4,9 +4,8 @@ macro equations(block)
     # Filter for assignments
     assignments = filter(e -> isa(e, Expr) && e.head == :(=), exprs)
 
-    # Extract variable names and their values
-    pairs = [(a.args[1] => a.args[2]) for a in assignments]
+    # Extract variable names and their values as symbols with colons
+    pairs = [Expr(:call, :(=>), :($(":$(a.args[1])")), a.args[2]) for a in assignments]
 
-    # Convert to a Dict expression
-    return :(Dict($pairs...))
+    return esc(Expr(:call, :Dict, pairs...))
 end
